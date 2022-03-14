c2163552df80e6bb2b8ae786bdf8117ce691f198
Sina Mahmoodi <itz.s1na@gmail.com>
2022-03-14 12:38:45 +0100

    turn App into class

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import jsonSchema from './full-schema.json';
 import './App.css';
 import 'graphiql/graphiql.min.css';
 
+const schemaUrl = 'https://github.com/s1na/graphql-schema-explorer/blob/5829ee7dd23175e7e5758b008e6904d3b8aee305/src/full-schema.json'
+
 const schema = buildClientSchema(jsonSchema);
 if (!validateSchema(schema)) {
     throw new Error("Invalid schema");
 }
 
-const App = () => <div className="graphiql-container"><div className="docExplorerWrap" style={{display: "block", width: "100%"}}><DocExplorer schema={schema} /></div></div>;
-
-export default App;
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className="graphiql-container">
+                <div className="docExplorerWrap" style={{display: "block", width: "100%"}}>
+                    <DocExplorer schema={schema} />
+                </div>
+            </div>
+        );
+    }
+}
